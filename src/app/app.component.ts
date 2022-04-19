df5b670b70362770ea4dc0465d620de64a9f88ba
mccafee025 <ademaamanka@gmail.com>
2022-04-19 10:17:20 +0300

    remove quotes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  hello = 'Top';
+  title = 'hello world';
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloworldComponent } from './helloworld/helloworld.component';
-import { QuotesComponent } from './quotes/quotes.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloworldComponent,
-    QuotesComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
