e80f3139bc8816dfa5a60b06d15d6802d51bc548
mccafee025 <ademaamanka@gmail.com>
2022-04-19 09:53:33 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloworldComponent } from './helloworld/helloworld.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloworldComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
